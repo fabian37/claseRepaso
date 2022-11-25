b699ef047928761b9a197e942518d38fc449d670
Fabian Gomez <fabian37gomez@gmail.com>
2022-11-25 01:10:29 -0500

    fix problem with css

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NewUser = ({ name, username, email, id }) => {
 	return (
 		<>
-			<div className="user">
+			<div className="newUser">
 				<h3>{name}</h3>
 				<p>{username}</p>
 				<p>{email}</p>
